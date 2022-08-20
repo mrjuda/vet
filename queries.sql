ae824c85fce61b92fcbf64a8e4cb82dde10e6f65
judah <87juda@gmail.com>
2022-08-20 16:47:36 -0300

    Add queries' screenshots to README

--- queries.sql
+++ queries.sql
@@ -1,11 +1,9 @@
-/* Write queries for the following quests: */
-
--- Find all animals whose name ends in "mon".
+-- Animals whose name ends in "mon".
 SELECT name
 FROM animals
 WHERE name LIKE '%mon';
 
--- List the name of all animals born between 2016 and 2019.
+-- Animals born between 2016 and 2019.
 SELECT name
 FROM animals
 WHERE
@@ -13,7 +11,8 @@ date_of_birth > '2015-12-31'
 AND
 date_of_birth < '2020-01-01';
 
--- List the name of all animals that are neutered and have less than 3 escape attempts.
+-- Animals that are neutered and have less
+-- than 3 escape attempts.
 SELECT name
 FROM animals
 WHERE
@@ -21,7 +20,8 @@ neutered = TRUE
 AND
 escape_attempts < 3;
 
--- List the date of birth of all animals named either "Agumon" or "Pikachu".
+-- Date of birth of all animals named either
+-- "Agumon" or "Pikachu".
 SELECT date_of_birth
 FROM animals
 WHERE
@@ -30,26 +30,30 @@ OR
 name = 'Pikachu';
 
 
--- List name and escape attempts of animals that weigh more than 10.5kg
+-- Name and escape attempts of animals
+-- that weigh more than 10.5kg
 SELECT name, escape_attempts
 FROM animals
 WHERE
 weight_kg > 10.5;
 
 
--- Find all animals that are neutered.
+-- Animals that are neutered.
 SELECT name
 FROM animals
 WHERE
 neutered = TRUE;
 
--- Find all animals not named Gabumon.
+-- Animals not named Gabumon.
 SELECT name
 FROM animals
 WHERE
 name NOT LIKE 'Gabumon';
 
--- Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)
+-- Animals with a weight between 10.4kg
+-- and 17.3kg (including animals with
+-- the weights that equals precisely
+-- 10.4kg or 17.3kg)
 SELECT name, weight_kg
 FROM animals
 WHERE (weight_kg >= 10.4 AND weight_kg <= 17.3);
